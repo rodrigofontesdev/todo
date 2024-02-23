5546c658b78c20d883d5c3efcd904ebb74edfbf2
Rodrigo Fontes <rodrigo_fnts@hotmail.com>
2024-02-23 12:24:54 -0300

    feat: add new task

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,80 @@
+import { PlusCircle } from "@phosphor-icons/react"
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from "react"
+import { Button } from "./components/Button"
 import { Empty } from "./components/Empty"
 import { Header } from "./components/Header"
-import { CreateTask } from "./components/CreateTask"
-import { Task } from "./components/Task"
+import { Input } from "./components/Input"
 import { Progress } from "./components/Progress"
+import { Task } from "./components/Task"
 
 import styles from "./App.module.css"
 import "./global.css"
 
+interface Task {
+  id: string
+  text: string
+  isDone: boolean
+}
+
 export function App() {
+  const [tasks, setTasks] = useState<Task[]>([])
+  const [taskText, setTaskText] = useState("")
+
+  function handleAddTask(event: FormEvent) {
+    event.preventDefault()
+
+    const newTask: Task = {
+      id: crypto.randomUUID(),
+      text: taskText,
+      isDone: false,
+    }
+
+    setTasks([...tasks, newTask])
+    setTaskText("")
+  }
+
+  function handleTaskTextChange(event: ChangeEvent<HTMLInputElement>) {
+    event.target.setCustomValidity("")
+
+    setTaskText(event.target.value)
+  }
+
+  function handleTaskTextOnInvalid(event: InvalidEvent<HTMLInputElement>) {
+    event.target.setCustomValidity("Esse campo é obrigatório!")
+  }
+
   return (
     <>
       <Header />
 
       <main>
         <div className={styles.container}>
-          <CreateTask />
+          <form className={styles.formCreateTask} onSubmit={handleAddTask}>
+            <Input
+              type="text"
+              name="taskText"
+              placeholder="Adicionar uma nova tarefa"
+              value={taskText}
+              onChange={handleTaskTextChange}
+              onInvalid={handleTaskTextOnInvalid}
+              required
+            />
+            <Button type="submit">
+              Criar <PlusCircle size={18} weight="bold" />
+            </Button>
+          </form>
 
           <Progress />
 
-          <Task content="Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer." />
-          <Task content="Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer." />
-          <Task content="Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer." />
-          <Task
-            content="Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer."
-            isDone={true}
-          />
-          <Task
-            content="Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer."
-            isDone={true}
-          />
-
-          {/* <Empty /> */}
+          {tasks.length > 0 ? (
+            tasks.map((task) => {
+              return (
+                <Task key={task.id} content={task.text} isDone={task.isDone} />
+              )
+            })
+          ) : (
+            <Empty />
+          )}
         </div>
       </main>
     </>
