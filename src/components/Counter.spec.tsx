f842ae3c0b62e8d0d3fb71aefe3ddd32b18c4e73
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-07-30 19:22:15 -0300

    test: test counter component

---
+++ src/components/Counter.spec.tsx
@@ -0,0 +1,17 @@
+import { render } from '@testing-library/react'
+import { Counter } from './Counter'
+
+describe('Counter', () => {
+  it('should display the right quantity of tasks', () => {
+    const wrapper = render(<Counter data={{ created: 3, done: 2 }} />)
+
+    const created = wrapper.getByText('3', {
+      exact: true,
+      selector: 'span',
+    })
+    const done = wrapper.getByText('2 de 3')
+
+    expect(created).toBeInTheDocument()
+    expect(done).toBeInTheDocument()
+  })
+})
