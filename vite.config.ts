c2ba8eafca207b76c325f203c61047fa5620becf
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-07-30 19:20:42 -0300

    build: install and configure vitest, testing-library and jsdom

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { type InlineConfig } from 'vitest'
+import { type UserConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    setupFiles: ['./src/test/setup.ts'],
+    environment: 'jsdom',
+  },
+} as UserConfig & {
+  test: InlineConfig
 })
