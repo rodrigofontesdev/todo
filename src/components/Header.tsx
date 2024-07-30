264fbba152b301bae80aa7f2908fe4d59e579b11
Rodrigo Fontes <fnts.rodrigo.dev@gmail.com>
2024-07-29 21:54:22 -0300

    refactor(component): improve responsive design of the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export function Header() {
   return (
     <header className={styles.mainHeader}>
       <div className={styles.container}>
-        <img src={logoImg} alt="Logo ToDo" />
+        <img src={logoImg} alt="Todo App" width={126} height={48} />
       </div>
     </header>
   )
